b6ff0420acd26e954214cc3095396b20f405fe1d
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-08-15 20:28:50 -0500

    I worked with Irving Duran to understand this code and how it all works. I finally feel like I have an understanding of how to make these components work.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,26 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const userName = 'stephentanksley'
+
+// Make a request for a user with a given ID
+axios.get('https://api.github.com/users/stephentanksley') // putting out the call to the API asking for information located at this URL.
+  .then(function (response) {
+    const info = response.data;
+    console.log(response.data)
+    console.log(response.data.message)
+    const newCard = gitHubCard(info.avatar_url, info.name, info.login, info.location, info.html_url, info.followers, info.following, info.bio)
+
+    const cards = document.querySelector('.cards')
+    cards.append(newCard)
+
+    console.log(info.login)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +44,31 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["RomeNineO", "smv5047", "farjadfazli", "fixmylifedesigns", "jennyobryant"];
+
+function LambdaUsers(Students) {
+  Students.map(student => {
+    axios.get(`https://api.github.com/users/${student}`) // putting out the call to the API asking for information located at this URL.
+      .then(function (response) {
+        const info = response.data;
+        console.log(response.data)
+        console.log(response.data.message)
+        const newCard = gitHubCard(info.avatar_url, info.name, info.login, info.location, info.html_url, info.followers, info.following, info.bio)
+
+        const cards = document.querySelector('.cards')
+        cards.append(newCard)
+        console.log(info.login)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  })
+}
+
+LambdaUsers(followersArray);
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +89,58 @@ const followersArray = [];
 </div>
 
 */
+
+function gitHubCard(imgSrc, Name, Username, Location, ProfileURL, Followers, Following, Bio) {
+
+  //Element creation
+  const div = document.createElement('div')
+  div.classList.add('card')
+
+  const image = document.createElement('img')
+  image.src = imgSrc
+  div.append(image)
+
+  const cardInfo = document.createElement('div')
+  cardInfo.classList.add('card-info')
+  div.append(cardInfo)
+
+  const name = document.createElement('h3')
+  name.classList.add('name')
+  name.textContent = Name;
+  cardInfo.append(name)
+
+  const userName = document.createElement('p')
+  userName.classList.add('username')
+  userName.textContent = Username
+  cardInfo.append(userName)
+
+  const homeTown = document.createElement('p')
+  homeTown.textContent = `Location: ${Location}`
+  cardInfo.append(homeTown)
+
+  const profile = document.createElement('p')
+  profile.textContent = "Profile: "
+  cardInfo.append(profile)
+
+  const profileLink = document.createElement('a')
+  profileLink.setAttribute('href', ProfileURL)
+  profileLink.textContent = 'Github'
+  profile.append(profileLink)
+
+  const followers = document.createElement('p')
+  followers.textContent = `Followers: ${Followers}`;
+  cardInfo.append(followers)
+
+  const following = document.createElement('p')
+  following.textContent = `Following: ${Following}`;
+  cardInfo.append(following)
+
+  const bio = document.createElement('p')
+  bio.textContent = `Bio: ${Bio}`
+  cardInfo.append(bio)
+
+  return div
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
